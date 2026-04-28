09f277b132de3d56d9df2724b49d8b2e5f3c45c4
Felipe Borges <lipelsborges@gmail.com>
2026-04-28 14:42:39 -0300

    .

--- 3_AVANÇANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANÇANDO_EM_REACT/avancando/src/App.jsx
@@ -3,11 +3,15 @@ import reactLogo from './assets/react.svg';
 // 2 - imagem assets
 import noite from './assets/reactimg2.png';
 
+//3 - useState
+
+import Data from '../components/Data';
+
 import './App.css'
 
 function App() {
   return (
-      <div className='App'>
+      <div className='App' style={{paddingBottom: "500px"}}>
         <h1>Avançando em React</h1>
         {/* 1 - imagem em public */}
         <img src="../public/reactimg22.avif" alt="Alguma imagem" />
@@ -16,6 +20,8 @@ function App() {
 
         <img src={noite} alt="Outra imagem" />
 
+        {/* 3 - useState */}
+          <Data/>
       </div>
   );
 }
