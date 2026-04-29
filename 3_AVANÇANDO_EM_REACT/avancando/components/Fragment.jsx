9cf1c61596b3d21fb66abd5a0c4ff77ec4ca5e11
Felipe Borges <lipelsborges@gmail.com>
2026-04-29 10:48:22 -0300

    .

---
+++ 3_AVANÇANDO_EM_REACT/avancando/components/Fragment.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Fragment = () => {
+  return (
+    <>
+        <div>
+            <h2>Temos dois elementos pai</h2>
+        </div>
+        <div>
+            <h2>Esse tbm é</h2>
+        </div>
+        
+    </>
+  )
+}
+
+export default Fragment
